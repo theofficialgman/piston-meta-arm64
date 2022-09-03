847ec400f03784142c81a8ae813f9f0235825db9
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2022-09-03 11:30:41 -0400

    add another version of lwjgl

--- mc-parse-script.sh
+++ mc-parse-script.sh
@@ -49,12 +49,12 @@ for json_file in `find . -type f -name "*.json"`; do
   fi
 
   # 3.3.1 3.2.2 3.2.1 3.1.6 3.1.2 2.9.4-nightly-20150209 2.9.3 2.9.1 2.9.1-nightly-20131120 2.9.0
-  if [[ "$lwjgl_version" =~ ^(2.9.4-nightly-20150209|2.9.3|2.9.1|2.9.1-nightly-20131120|2.9.0)$ ]]; then
+  if [[ "$lwjgl_version" =~ ^(2.9.4-nightly-20150209|2.9.3|2.9.1|2.9.1-nightly-20130708-debug3|2.9.1-nightly-20131120|2.9.0)$ ]]; then
     spruce merge "$GIT_DIR/version-json/$json_file" "$GIT_DIR/mc-static-json/2.9.4-nightly-20150209.json" | spruce json | sponge "$GIT_DIR/version-json/$json_file"
   elif [[ "$lwjgl_version" =~ ^(3.1.2|3.1.6|3.2.1|3.2.2|3.3.1)$ ]]; then
     spruce merge "$GIT_DIR/version-json/$json_file" "$GIT_DIR/mc-static-json/$lwjgl_version.json" | spruce json | sponge "$GIT_DIR/version-json/$json_file"
   else
-    echo "This version of LWJGL ($lwjgl_version) is not recognized"
+    echo -e "\e[91mThis version of LWJGL ($lwjgl_version) is not recognized\e[0m"
   fi
   
 done
