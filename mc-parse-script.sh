ca243f78b536fee39ada87e03e89251744a3b430
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2026-07-27 22:34:34 -0400

    adjust lwjgl version parser for new format

--- mc-parse-script.sh
+++ mc-parse-script.sh
@@ -31,7 +31,7 @@ for json_file in `find . -type f -name "*.json"`; do
   # cat "$json_file" | jq > "../original_json_jq/$json_file"
   
   # "name": "org.lwjgl:lwjgl:3.3.1"
-  lwjgl_version=$(cat "$json_file" | jq -r '.libraries | .[] | .name | select(contains("org.lwjgl:lwjgl:"))' | sed 's/org.lwjgl:lwjgl://g' | sed '/:/d' | sort -g -r | head -1)
+  lwjgl_version=$(cat "$json_file" | jq -r '.libraries | .[] | .name | select(contains("org.lwjgl:lwjgl:"))' | sed 's/org.lwjgl:lwjgl://g' | sed 's/:.*//' | sort -g -r | head -1)
   if [ -z "$lwjgl_version" ];then
     # "name": "org.lwjgl.lwjgl:lwjgl:2.9.1-nightly-20131120"
     lwjgl_version=$(cat "$json_file" | jq -r '.libraries | .[] | .name | select(contains("org.lwjgl.lwjgl:lwjgl:"))' | sed 's/org.lwjgl.lwjgl:lwjgl://g' | sed '/:/d' | sort -g -r | head -1)
